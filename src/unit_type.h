e1118b12cd62cd5cbbcdf92836321a5a387a097e
Huxlyx <mtengler@gmail.com>
2025-11-01 00:45:39 +0100

    added channel id

--- src/unit_type.h
+++ src/unit_type.h
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 
+const uint8_t UNKNOWN               = 0x00;
+
 const uint8_t DEVICE_ID             = 0x01;
 const uint8_t MEASUREMENT_REQ       = 0x04;
 const uint8_t MEASUREMENT_RESP      = 0x05;
 const uint8_t TOGGLE_OUT_PIN        = 0x06;
 const uint8_t DEVICE_SETTING_REQ    = 0x0C;
-const uint8_t DEVICE_SETTING_SET    = 0x0D;
+const uint8_t DEVICE_SETTING_CHANGE = 0x0D;
 const uint8_t ACK                   = 0x0E;
 const uint8_t RESET                 = 0x0F;
 
@@ -19,6 +21,7 @@ const uint8_t HUMIDITY              = 0xB3;
 const uint8_t LIGHT                 = 0xB4;
 const uint8_t DISTANCE_MM           = 0xB5;
 const uint8_t DISTANCE_ABS          = 0xB6;
+const uint8_t CO2_PPM               = 0xB7;
 const uint8_t UP_TIME               = 0xBA;
 const uint8_t IMAGE                 = 0xBE;
 
@@ -28,6 +31,24 @@ const uint8_t END_SEGMENT   = 0xC2;
 
 const uint8_t UDP_BROADCAST_DELAY   = 0xD0;
 const uint8_t CONNECTION_DELAY      = 0xD1;
+const uint8_t IN_PIN_MASK           = 0xD2;
+const uint8_t OUT_PIN_MASK          = 0xD3;
 
 const uint8_t STATUS_MSG    = 0xF0;
 const uint8_t ERROR         = 0xFF;
+
+/* I/O channel mask*/
+
+const uint8_t IN_CHANNEL_0_EN = 0x01;
+const uint8_t IN_CHANNEL_1_EN = 0x02;
+const uint8_t IN_CHANNEL_2_EN = 0x04;
+const uint8_t IN_CHANNEL_3_EN = 0x08;
+const uint8_t IN_CHANNEL_4_EN = 0x10;
+const uint8_t IN_CHANNEL_5_EN = 0x20;
+const uint8_t IN_CHANNEL_6_EN = 0x40;
+const uint8_t IN_CHANNEL_7_EN = 0x80;
+
+const uint8_t OUT_CHANNEL_0_EN = 0x01;
+const uint8_t OUT_CHANNEL_1_EN = 0x02;
+const uint8_t OUT_CHANNEL_2_EN = 0x04;
+const uint8_t OUT_CHANNEL_3_EN = 0x08;
